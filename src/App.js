97f9b040d81dfb06defbd1c1b419b560b17dd46e
CodeXRook <lacekitfly@gmail.com>
2019-02-24 16:48:14 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import moment from 'moment';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { HashRouter, Route, Link } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello world</h1>
+      <HashRouter>
+      <>
+        <Route path='/aboutus' component={Aboutus} />
+        <Route path='/' exact component={Home} />
+        
+       
+      </>
+    </HashRouter>
     );
   }
 }
 
 export default App;
-Y
