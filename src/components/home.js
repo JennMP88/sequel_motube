e078a96bcf3f9af297ddfe496f2688d51ed9e6a3
CodeXRook <lacekitfly@gmail.com>
2019-02-25 20:47:56 -0500

    JumboTron added

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,13 @@ class Home extends React.Component{
         return(
             <>
             <div className='home'>
-            this home page
+            <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
+
             </div>
 
             </>
