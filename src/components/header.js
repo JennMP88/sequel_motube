3e6fd461d9650830df49c6a8ad99157ed6698049
CodeXRook <lacekitfly@gmail.com>
2019-02-26 04:57:19 -0500

    tried new classes

--- src/components/header.js
+++ src/components/header.js
@@ -6,28 +6,29 @@ const Navbar = props => {
   return (
     <>
       <div class="flex-container">
-      <div className='Navbar'>
-        <nav class="navbar navbar-expand-lg ">
-        <img width = '30'  src= { require('../assets/image.jpeg')} />
 
-          <a className='mainTitle' class=" navbar-brand " href="#"> Pursuit Tube </a>
-          <a className="home" class="nav-item nav-link active" href="#"> Home <span class="sr-only"> (current)</span> </a>
-          <a className='user' class="nav-item nav-link" href="#">User</a>
+        <div className='Navbar'>
 
-          <a className='feedEdit' class="nav-item nav-link" href="#">Feed Editor</a>
-          
+          <nav class="navbar navbar-expand-lg ">
+            <img width='20' src={require('../assets/image.jpeg')} />
 
-          <input class="box" type="search" id="search" placeholder="Search" />
-
-          <button className='searchButton' type='text' placeholder='Search ...' >Search</button>
-
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-
-            <div class="navbar-nav">
-         
+            <div class="navbar">
+              <a href="#"> Pursuit Tube</a>
+              <a href="#"> User</a>
+              <a href="#"> Feed Editor </a>
             </div>
-          </div>
-        </nav>
+
+            <input class="box" type="search" id="search" placeholder="Search" />
+
+            <button className='searchButton' type='text' placeholder='Search ...' >Search</button>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+              <div class="navbar-nav">
+
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
     </>
