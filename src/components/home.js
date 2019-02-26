6bceb9f4b010e1f60c1657d041619c4bd98ed180
CodeXRook <lacekitfly@gmail.com>
2019-02-26 01:16:45 -0500

    homm css

--- src/components/home.js
+++ src/components/home.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import './home.css'
 
 
-class Home extends React.Component{
-    constructor(){
+class Home extends React.Component {
+    constructor() {
         super()
     }
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <div className='home'>
-            <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Fluid jumbotron</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-  </div>
-</div>
+                <div className='home'>
+                    <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                            <h1 class="display-4">Mo's Feed</h1>
+                            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        </div>
+                    </div>
 
-            </div>
+                </div>
 
             </>
         )
