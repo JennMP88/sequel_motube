3d0aa0223bffbdb123aeaa5d4ad3a908c89bd3c0
CodeXRook <lacekitfly@gmail.com>
2019-02-26 01:28:43 -0500

    Working on home styling

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
                 <div className='home'>
                     <div class="jumbotron jumbotron-fluid">
                         <div class="container">
-                            <h1 class="display-4">Mo's Feed</h1>
+                            <h1 class="display-4"> Steve's Feed</h1>
                             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
                         </div>
                     </div>
