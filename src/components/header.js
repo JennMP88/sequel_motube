9a2d226c4480f22981ec55f228f3cb97f11b9521
CodeXRook <lacekitfly@gmail.com>
2019-02-26 01:52:39 -0500

    went over header css

--- src/components/header.js
+++ src/components/header.js
@@ -12,8 +12,8 @@ const Navbar = props => {
           <a className="home" class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
           <a className='user' class="nav-item nav-link" href="#">User</a>
           <a className='feedEdit' class="nav-item nav-link" href="#">Feed Editor</a>
-          <input className='searchInput' type='text' placeholder='Search ...' ></input>
-          <button className='searchButton' type='text' placeholder='Search ...' >Search...</button>
+          <input className='searchInput' type='text' placeholder='Search ' ></input>
+          <button className='searchButton' type='text' placeholder='Search ...' >Search</button>
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
 
