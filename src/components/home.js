6b03f485f7f741eb50ed379df312ca46f6536c33
CodeXRook <lacekitfly@gmail.com>
2019-02-26 03:14:08 -0500

    working on body

--- src/components/home.js
+++ src/components/home.js
@@ -9,15 +9,28 @@ class Home extends React.Component {
     render() {
         return (
             <>
-            <div class="container">
                 <div className='Jumbo'>
                     <div class="jumbotron jumbotron-fluid">
-                            <h1 class="display-4">User Playlist</h1>
-                        </div>
+                        <h1 class="display-4">User Playlist</h1>
                     </div>
-
                 </div>
 
+                <body>
+                       <div class="container-fluid"> 
+                       <h2 class="display-4">Feed List</h2>
+                       <div class="row-fluid">
+                       
+                       
+                        <div class="span2">
+                            {/* <!--Sidebar content--> */}
+                            </div>
+                           <div class="span10">
+                          
+                            {/* <!--Body content--> */}
+                            </div>
+                    </div>
+                </div>
+         </body>
             </>
         )
     }
