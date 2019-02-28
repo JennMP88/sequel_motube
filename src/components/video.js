12769adca7470caa71174225aa5375060cf300ff
CodeXRook <lacekitfly@gmail.com>
2019-02-28 13:50:02 -0500

    improved boxed components

--- src/components/video.js
+++ src/components/video.js
@@ -2,28 +2,42 @@ import React from 'react';
 import './video.css';
 
 class Video extends React.Component {
-        constructor(props){
-          super(props);
-          this.state = {
-            
-          }
- }
+    constructor(props) {
+        super(props);
+        this.state = {
 
-render(){
-    return(
-              <>
-         <div class="main" >
-           <div class="row" >
-            <h2>Espn First Take</h2>
+        }
+    }
 
-            
-            <div class="col-sm" >
-            </div>
-         </div>
-    </div>
-           </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <div class="main" >
+                    <div class="row" >
+                        <h2>Espn First Take</h2>
+                        <div class="col-sm" >
+                        </div>
+                    </div>
+                </div>
+                   
+                   <div>
+                    <div class="container">
+                        <div class="dropdown-menu"> <h2> '' </h2></div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <div class="side">
+                                    <h2>Feed list</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </>
+        );
+    }
 }
 
 export default Video;
