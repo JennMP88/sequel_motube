b7f4d91e949ff7cdd0ae1978a3a89ced181744ed
CodeXRook <lacekitfly@gmail.com>
2019-02-28 02:59:30 -0500

    updated constructor in video js

--- src/components/video.js
+++ src/components/video.js
@@ -1,12 +1,18 @@
-// import React from 'react';
+import React from 'react';
 
-// const VideoPlayer = ({ id }) =>{
-//  const link = `https://www.youtube.com/${id}?autoplay=1&fs=1&origin=http://localhost:3000`
+class Video extends React.Component{
+    constructor () {
+        super()
+    }
+}
 
-//  return(
-//      <iframe title='yt-video' type="text/html" width="640" height="360">
-//     src=[link] frameBorder="0"></iframe>
-//    );
-//  }
+const VideoPlayer = ({ id }) =>{
+ const link = `https://www.youtube.com/${id}?autoplay=1&fs=1&origin=http://localhost:3000`
+
+ return(
+     <iframe title='yt-video' type="text/html" width="640" height="360">
+    src=[link] frameBorder="0"></iframe>
+   );
+ }
  
-//  export default VideoPlayer;
+ export default VideoPlayer;
