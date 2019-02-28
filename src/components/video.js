3d955933f3a8568e7dbdce8b243437df09245f1f
CodeXRook <lacekitfly@gmail.com>
2019-02-28 02:53:59 -0500

    need to text a video

--- src/components/video.js
+++ src/components/video.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const VideoPlayer = 'react';
+const VideoPlayer = ({ id }) =>{
  const link = `https://www.youtube.com/${id}?autoplay=1&fs=1&origin=http://localhost:3000`
 
  return(
-     <iframe title='yt-video' type="text/html"
- )
+     <iframe title='yt-video' type="text/html" width="640" height="360">
+    src=[link] frameBorder="0"></iframe>
+ );
+ }
+ 
+ export default VideoPlayer;
