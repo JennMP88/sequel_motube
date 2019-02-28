fdd7f59b08bc025588e16262430a63ffee16b1d0
CodeXRook <lacekitfly@gmail.com>
2019-02-28 04:39:17 -0500

    looking to get video to work

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Video from './components/video';
 import './home.css'
 
 
@@ -16,15 +17,10 @@ class Home extends React.Component {
                     </div>
                 </div>
 
-                <div class="clearfix">
-                    <div class="spinner-border float-right" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                </div>
 
 
                 <div class="container">
-                    <div class="dropdown-menu"></div>
+                    <div class="dropdown-menu"> <h2> '' </h2></div>
                     <div class="row">
                         <div class="col-sm">
                             <div class="side">
@@ -34,14 +30,24 @@ class Home extends React.Component {
                     </div>
                 </div>
 
-
                 <div class="main">
                     <div class="row">
                         <h2>Espn First Take</h2>
                         <div class="col-sm">
+
+
+                        <div class="text-center">
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+
+
                         </div>
                     </div>
                 </div>
+
+
             </>
         )
     }
