2751d1353edbfa8d9faf1dd4667149b621f06de5
Ivy Strickland <IvyStrickland@pursuit.org>
2019-02-28 21:59:20 -0500

    added thumbnails

--- src/components/video.js
+++ src/components/video.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-const VideoPlayer = ({ id, title }) => {
+const VideoPlayer = (props) => {
+  const {id, title, channel, posted, thumbnail} = props.video
   const link = `https://www.youtube.com/embed/${id}?autoplay=1&fs=1&origin=http://localhost:3000`;
 
   return (
 
   
   <div className='col-3'>
-        <div>
+        {/* <div>
           <iframe title='yt-video' type="text/html" width="200" height="120"
           src={link} frameBorder="0"></iframe>
-        </div>
+        </div> */}
+        <img src={thumbnail} alt=''></img>
         {title}
+        {channel}
+        {posted}
       </div>
     
   
