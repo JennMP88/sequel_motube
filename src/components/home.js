f11bce3c588fd5ca850188e16bb37e6a86ab6bd3
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-03 09:54:00 -0500

    steve's home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import VideoPlayer from './video';
-import axios from 'axios'; 
+import Video from './video';
+import axios from 'axios';
 import './home.css'
 
 
@@ -10,19 +10,20 @@ class Home extends React.Component {
         this.state = {
             feedlist: [
                 {
-                    query: 'Bethel',
+                    query: 'ESPN FIRST TAKE',
                     videos: [
                         {
-                            title: '',
+                            title: 'La Bron James',
                             thumbnail: '',
                             channel: '',
                             posted: '',
                             id: '',
-    
                         },
+                        
                     ]
                 },
             ]
+
 
 
         }
@@ -33,89 +34,144 @@ class Home extends React.Component {
             method: 'get',
             url: 'https://www.googleapis.com/youtube/v3/search',
             params: {
-              part: 'snippet',
-              maxResults: 8,
-              videoDefinition: 'high',
-              type: 'video',
-              videoEmbeddable: 'true',
-              key: 'AIzaSyAL6JKGb4TLsmM4MKvk0f85Bu4QpaJvgEs',
-              q: query,
-              pageToken: ''
+                part: 'snippet',
+                maxResults: 8,
+                videoDefinition: 'high',
+                type: 'video',
+                videoEmbeddable: 'true',
+                key: 'AIzaSyBqsTC2uJ5zklkPcEDeLpAz4B6UktMYuPo',
+                q: query,
+                pageToken: ''
             }
-          })
-          .then(res=>res.data)
-          .then((data)=>{
-              console.log(data.items)
-            let feedCopy = [...this.state.feedlist]
-            feedCopy[0].videos[0].title = data.items[0].snippet.title
-            feedCopy[0].videos[0].thumbnail = data.items[0].snippet.thumbnails.medium.url
-            feedCopy[0].videos[0].channel = data.items[0].snippet.channelTitle
-            feedCopy[0].videos[0].posted = data.items[0].snippet.publishedAt
-            feedCopy[0].videos[0].id = data.items[0].id.videoId
-            this.setState({feedlist: feedCopy})
-          })
+        })
+            .then(res => res.data)
+            .then((data) => {
+                let feedCopy = [...this.state.feedlist]
+                feedCopy[0].videos[0].title = data.items[0].snippet.title
+                feedCopy[0].videos[0].thumbnail = data.items[0].snippet.thumbnails.medium.url
+                feedCopy[0].videos[0].channel = data.items[0].snippet.channelTitle
+                feedCopy[0].videos[0].posted = data.items[0].snippet.publishedAt
+                feedCopy[0].videos[0].id = data.items[0].id.videoId
+                this.setState({ feedlist: feedCopy })
+            })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let query = this.state.feedlist[0].query
-        this.getVideos(this.state.feedlist[0].query)
+        this.getVideos(query)
     }
+
+    
 
     render() {
         return (
             <>
 
-                <div className='row' >
+                <div className='Container' >
                     <div className='Jumbo'>
-                        <div class="jumbotron jumbotron-fluid">
-                            <h1 class="display-4">User Playlist</h1>
+                        <div className="jumbotron jumbotron-fluid">
+                            <h1 className="display-4">User's Playlist </h1>
                         </div>
                     </div>
 
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-4">
-                                <div className='fl' class="side">
-                                    <h2>Feed list</h2>
+
+                    <div className="container">
+
+                        <div className="row feed">
+
+                            <div className="col-2">
+
+                                <h1>Feed list</h1>
+
+                                <div className="list-group">
+                                    <a href="#" className="list-group-item list-group-item-action">ESPN First Take</a>
+                                    <a href="#" className="list-group-item list-group-item-action">Drake</a>
+                                    <a href="#" className="list-group-item list-group-item-action">Coldfusion</a>
                                 </div>
                             </div>
-                            <div class="col-8">
-                                <div className='title' class="main">
-                                    <h2>TITLE HEADING</h2>    {/* PUT TITLE IN MAP  */}
-                                </div>
-                                <div className='fl' class="side">
-                                {
-                                    this.state.feedlist.map((feed,i) => {
-                                       return <div className='row' >
-                                      
 
+                          <div className="feedList">
+                            <div className="col-10"> 
+                            
+                                    <div className='title' className="main">
+
+                                        <h2> Espn First Take</h2>
+                                    </div>
+                                </div>
+
+                                <div className='fl' className="side">
                                     {
-                                        feed.videos.map((video) => {
-                                            return <VideoPlayer video = {video}/>
-                                        } )
+                                        this.state.feedlist.map((feed, i) => {
+                                            return <div className='row' >
+
+                                                {
+                                                    feed.videos.map((video) => {
+                                                        return <Video video={video} />
+                                                    })
+                                                }
+
+                                            </div>
+                                        })
                                     }
+                                <button type="button" class="btn btn-outline-dark">Show more</button>
+                                </div>
+                     
 
-                                        </div>
-                                    })
-                                }
+                            <div className="col-10">
+                           
+                                    <div className='title' className="main">
 
-                                
-                                    
-                                    
-                                       
-                                    
-
+                                        <h3> Drake</h3>
+                                    </div>
                                 </div>
 
+                                <div className='fl' className="side">
+                                    {
+                                        this.state.feedlist.map((feed, i) => {
+                                            return <div className='row' >
+
+                                                {
+                                                    feed.videos.map((video) => {
+                                                        return <Video video={video} />
+                                                    })
+                                                }
+
+                                            </div>
+                                        })
+                                    }
+                             <button type="button" class="btn btn-outline-dark">Show more</button>
+                                </div>
+                           </div>
+
+
+                            <div className="col-10">
+                                
+                                    <div className='title' className="main">
+
+                                        <h4>Coldfusion</h4>
+                                    </div>
+                                 </div>
+
+                                <div className='fl' className="side">
+                                    {
+                                        this.state.feedlist.map((feed, i) => {
+                                            return <div className='row' >
+
+                                                {
+                                                    feed.videos.map((video) => {
+                                                        return <Video video={video} />
+                                                    })
+                                                }
+
+                                            </div>
+                                        })
+                                    }
+                            <button type="button" class="btn btn-outline-dark">Show more</button>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
-
-
-                </div>
-
-
-
             </>
         )
     }
