3c2bace59cba90befd1b42f2da274b8151882dc7
Senama <sergeenama@gmail.com>
2019-03-03 09:13:56 -0500

    added searchbar

---
+++ src/components/search.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react'
+import { Link, withRouter } from 'react-router-dom';
+import axios from 'axios'
+import Home from './home'
+import Home from './home.css'
+import Header from './header'
+
+
+
+class Search extends Component {
+    constructor(props){
+        super(props)
+        this.state = {isLoading:true,data:[],pageToken:'',hover:false}
+    }
+
+    getVideoList = (query,pageToken='')=>{
+        axios({ 
+            method: 'get',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                videoDefinition: 'high',
+                type: 'video',
+                videoEmbeddable: 'true',
+                key: 'AIzaSyAqwvNBfv73cn5RuP-VoRrMpekbCqStUTY',
+                q: query,
+                pageToken: pageToken
+            }
+        })
+        .then(response=>{
+            const videoListData = []
+            response.data.items.forEach(vid=>{
+                let currentLoad = {}
+                currentLoad.video_id = vid.id.videoId  //video id
+                currentLoad.img = vid.snippet.thumbnails.medium.url //img url
+                currentLoad.title = vid.snippet.title //title
+                currentLoad.channel_title = vid.snippet.channelTitle
+                currentLoad.descrip = vid.snippet.description
+                currentLoad.date = vid.snippet.publishedAt
+                videoListData.push(currentLoad)
+            })
+            //console.log(response.data.nextPageToken)
+            let nextLoad = this.state.data.concat(videoListData)
+            let obj = {vids: nextLoad};
+            let suggestions = JSON.parse(localStorage.getItem('suggestions'));
+            if (!suggestions) {
+                localStorage.setItem('suggestions',JSON.stringify(obj))
+            }
+            else {
+                suggestions.vids = suggestions.vids.concat(obj.vids)
+                localStorage.setItem('suggestions',JSON.stringify(suggestions))
+            }
+            this.setState({isLoading:false,data:nextLoad,pageToken:response.data.nextPageToken})
+        })
+    }
+
+
+    componentDidMount(){
+        this.getVideoList(this.props.match.params.search_term)
+        window.addEventListener('scroll', this.handleOnScroll(this.props.match.params.search_term))
+        window.addEventListener('scroll', this.handleOnScroll);
+    }
+
+
+    componentWillReceiveProps(newProps){
+        this.setState({data:[]})
+        this.getVideoList(newProps.match.params.search_term)
+        window.addEventListener('scroll', this.handleOnScroll)
+        
+    }
+
+    handleOnScroll = () => {
+
+        const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+        const clientHeight = document.documentElement.clientHeight || window.innerHeight;
+        const scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+    
+    
+        if(scrolledToBottom) {
+          setTimeout(this.getVideoList(this.props.match.params.search_term,this.state.pageToken), 2000 )
+          window.scrollTo(0,5000)
+        }
+    }
+
+    render() {
+        if(this.state.isLoading) return <div className="spinner-border text-info" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+        else{ 
+            return <>
+            <div className="jumbotron">
+            <div style={{marginLeft:'10%'}}>
+            {this.state.data.map((vid, i)=>{
+                return <div className='hover row' key={i}>
+                <div className='col-4'>
+                    <Link to={`/video/${vid.video_id}`}><img onClick={e=>this.addHistory(vid)} src={vid.img} alt={vid.img} /></Link>
+                </div>
+                <div className='col-6'>
+                    <Link className='link_name' to={`/video/${vid.video_id}`}>{vid.title}</Link>
+                    <p>{vid.channel_title}</p>
+                    <p>{vid.descrip}</p>
+                    <p>{this.relativeTime(vid.date)}</p>
+                </div>
+                </div>
+
+            })}
+            <button onClick={e=>{this.getVideoList(this.props.match.params.search_term,this.state.offset+10)}}>Load More</button>
+            </div>
+            </div>
+            </>
+            
+        }     
+    }   
+}
+
+
+export default withRouter(Search)
